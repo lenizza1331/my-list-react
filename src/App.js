2580024c82e982d3da0fb2e7c18e26b766b885b3
lenizza1331 <leniyuva@gmail.com>
2022-02-27 21:03:19 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { MyList } from './MyList';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+          <div className="container__item">
+              <h1>Happy person planner</h1>
+              <p>Isn't it a great day to try something new?</p>
+          </div>
+          
+          <MyList />
+      </div>
   );
 }
 
---
+++ src/MyList.js
@@ -0,0 +1,73 @@
+import { Component } from "react";
+
+export class MyList extends Component {
+    state = {
+        userInput: '',
+        myList: ["Try new food", "Walk to new place"]
+    }
+    
+    changeInput(e){
+        this.setState({userInput: e})
+    }
+
+    addItem(input){
+        if (input === ""){
+            alert('Please add an item')
+        }
+        else {
+        let fullList = this.state.myList;
+        fullList.push(input);
+        this.setState({myList: fullList, userInput: ''})  
+        }  
+    }
+
+    crossItem(event){
+        const li = event.target;
+        li.classList.toggle("crossed");
+    }
+
+    deleteAll(){
+        this.setState({myList:[]})
+    }
+
+    submitAdd(e){
+        e.preventDefault();
+    }
+
+
+    render () {
+        return (
+            
+                <form onSubmit={this.submitAdd}>
+                <div className="container__item__list">
+                <input
+                    placeholder="Today I want to..."
+                    type="text"
+                    onChange={(e) => {
+                        this.changeInput(e.target.value);
+                    }}
+                    value={this.state.userInput} />
+                </div>
+
+                <div className="container__item__list" >
+                    <button className="btn_add" onClick={()=>this.addItem(this.state.userInput)}>
+                        Add
+                    </button>
+                </div>
+
+                <div className="container__item__list">
+                    <button className="btn_clear" onClick={()=>this.deleteAll()}>Clear all</button>
+                </div>
+
+                <div className="container__item__list">
+                    <ul>
+                        {this.state.myList.map((i, index)=>(
+                            <li onClick={this.crossItem} key={index}>{i}</li>
+                        ))}
+                    </ul>
+                </div>
+                </form>
+            
+        )
+    }
+}
